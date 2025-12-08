ecadac2ba558cfc58fb54427ee774466021a4e55
nmolokw1 <nmolokw1@students.towson.edu>
2025-12-08 16:05:28 -0500

    Overwriting buggy main branch + dialog for scheduled workout

--- app/src/main/java/com/example/regiment/ui/theme/screens/SchedulingScreen.kt
+++ app/src/main/java/com/example/regiment/ui/theme/screens/SchedulingScreen.kt
@@ -2,26 +2,40 @@ package com.example.regiment.ui.theme.screens
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerColors
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.regiment.R
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -29,6 +43,8 @@ import com.example.regiment.R
 @Composable
 fun SchedulingScreen(navController: NavHostController) {
     val state = rememberDatePickerState()
+    val openDialog = remember {mutableStateOf(false)}
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -53,16 +69,47 @@ fun SchedulingScreen(navController: NavHostController) {
     { padding ->
 
 
-        Column (modifier = Modifier.padding(padding)){
+        Column (modifier = Modifier.padding(padding), horizontalAlignment = Alignment.CenterHorizontally){
+
             DatePicker(
                 state = state,
                 showModeToggle = true,
                 title = {
                     Text("Select Workout Date",
-                    modifier = Modifier.padding(16.dp))
+                    modifier = Modifier.padding(20.dp))
                 }
             )
-//            TODO("Section for selecting the type of workout scheduled")
+            Button(onClick = {
+                openDialog.value = true })
+            { Text("Assign Workout") }
+
+            if(openDialog.value){
+                BasicAlertDialog(
+                    onDismissRequest = {
+                        openDialog.value = false
+                    }
+                ){
+                    Surface {
+                        Column(modifier = Modifier.padding(16.dp)) {
+                            Text(
+                                text =
+                                    "Ok, workout scheduled for \n${state.selectedDateMillis.let{DatePickerDefaults.dateFormatter().formatDate(it,
+                                        locale = Locale.current.platformLocale
+                                    )}}")
+                            Spacer(modifier = Modifier.height(24.dp))
+                            TextButton(
+                                onClick = { openDialog.value = false },
+                                modifier = Modifier.align(Alignment.End)
+                            ) {
+                                Text("Confirm")
+                            }
+                        }
+                    }
+                }
+            }
+//
+        //
+        //            TODO("Section for selecting the type of workout scheduled")
 //            TODO("Moving it all to a vm ;-; ")
         }
 
@@ -72,6 +119,6 @@ fun SchedulingScreen(navController: NavHostController) {
 @Preview
 @Composable
 fun SchedulingScreenPreview(){
-//    SchedulingScreen()
+    SchedulingScreen(navController = rememberNavController())
 }
 
